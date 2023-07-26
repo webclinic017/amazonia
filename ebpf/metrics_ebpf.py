803e040e5e7e4e531561b28eff8b1e4fdf317d5d
Hudson Coutinho <49199914+Hudsoncoutinho@users.noreply.github.com>
2023-07-25 21:42:06 -0300

    Update metrics_ebpf.py

--- ebpf/metrics_ebpf.py
+++ ebpf/metrics_ebpf.py
@@ -1,43 +1,54 @@
-<!-- Arquivo: index.html -->
-<html>
-<head>
-    <title>Server Metrics</title>
-</head>
-<body>
-    <h1>Server Metrics</h1>
-    <button onclick="fetchMetrics()">Fetch Metrics</button>
-    <ul id="metricsList"></ul>
+# Arquivo: metrics_ebpf.py
+from bcc import BPF
+from time import sleep
 
-    <script>
-        async function fetchMetrics() {
-            try {
-                const response = await fetch('/metrics');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok.');
-                }
-                const metricsData = await response.json();
-                displayMetrics(metricsData);
-            } catch (error) {
-                console.error('Error fetching metrics:', error);
-            }
-        }
+bpf_code = """
+#include <linux/bpf.h>
+#include <linux/ptrace.h>
+#include <linux/sched.h>
+#include <linux/net.h>
+#include <uapi/linux/ptrace.h>
 
-        function displayMetrics(metricsData) {
-            const metricsList = document.getElementById('metricsList');
-            metricsList.innerHTML = '';
+struct data_t {
+    u64 pid;
+    char comm[TASK_COMM_LEN];
+    u64 cpu_usage;
+    u64 memory_usage;
+    u64 rx_bytes;
+    u64 tx_bytes;
+    u32 local_port;
+};
 
-            for (const metric of metricsData) {
-                const cpuUsage = (metric.cpu_usage / 1e6).toFixed(2); // Convert ns to ms
-                const memoryUsage = (metric.memory_usage / (1024 * 1024)).toFixed(2); // Convert bytes to MB
-                const rxBytes = metric.rx_bytes;
-                const txBytes = metric.tx_bytes;
-                const localPorts = Array.from(metric.local_ports).join(', ');
+BPF_PERF_OUTPUT(events);
 
-                const metricInfo = document.createElement('li');
-                metricInfo.textContent = `Process ${metric.comm} (PID ${metric.pid}): CPU Usage: ${cpuUsage} ms, Memory Usage: ${memoryUsage} MB, Rx Bytes: ${rxBytes}, Tx Bytes: ${txBytes}, Local Ports: ${localPorts}`;
-                metricsList.appendChild(metricInfo);
-            }
-        }
-    </script>
-</body>
-</html>
+int monitor_process(struct pt_regs *ctx) {
+    struct data_t data = {};
+
+    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+    data.pid = task->tgid;
+    bpf_get_current_comm(&data.comm, sizeof(data.comm));
+    data.cpu_usage = bpf_ktime_get_ns();
+    data.memory_usage = task->mm ? task->mm->total_vm << (PAGE_SHIFT - 10) : 0;
+
+    struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM2(ctx);
+    struct iphdr *ip = bpf_hdr_pointer(skb, 0);
+    data.rx_bytes = ip ? skb->len : 0;
+
+    struct tcphdr *tcp = (struct tcphdr *)(ip + 1);
+    data.tx_bytes = tcp ? skb->len - (ip->ihl << 2) - (tcp->doff << 2) : 0;
+    data.local_port = tcp ? ntohs(tcp->source) : 0;
+
+    events.perf_submit(ctx, &data, sizeof(data));
+    return 0;
+}
+"""
+
+bpf = BPF(text=bpf_code)
+bpf.attach_kprobe(event="finish_task_switch", fn_name="monitor_process")
+
+# Infinite loop to keep the eBPF program running
+try:
+    while True:
+        bpf.perf_buffer_poll()
+except KeyboardInterrupt:
+    pass
