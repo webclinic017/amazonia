b074abe3d9785d29ef8453d78f49d6af8c14048e
Hudson Coutinho <49199914+Hudsoncoutinho@users.noreply.github.com>
2023-07-27 17:54:55 -0300

    Update metrics_ebpf.py

--- ebpf/metrics_ebpf.py
+++ ebpf/metrics_ebpf.py
@@ -1,54 +1,42 @@
-# Arquivo: metrics_ebpf.py
+import time
 from bcc import BPF
-from time import sleep
+from bcc.utils import printb
 
-bpf_code = """
-#include <linux/bpf.h>
-#include <linux/ptrace.h>
-#include <linux/sched.h>
-#include <linux/net.h>
-#include <uapi/linux/ptrace.h>
 
-struct data_t {
-    u64 pid;
-    char comm[TASK_COMM_LEN];
-    u64 cpu_usage;
-    u64 memory_usage;
-    u64 rx_bytes;
-    u64 tx_bytes;
-    u32 local_port;
-};
+def process_event(cpu, data, size):
 
-BPF_PERF_OUTPUT(events);
+def ports_event(cpu, data, size):
 
-int monitor_process(struct pt_regs *ctx) {
-    struct data_t data = {};
+b = BPF(text=ebpf_code_processes)
+b.attach_kprobe(event=b.get_syscall_fnname("execve"), fn_name="trace_process")
 
-    struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-    data.pid = task->tgid;
-    bpf_get_current_comm(&data.comm, sizeof(data.comm));
-    data.cpu_usage = bpf_ktime_get_ns();
-    data.memory_usage = task->mm ? task->mm->total_vm << (PAGE_SHIFT - 10) : 0;
+b_ports = BPF(text=ebpf_code_ports)
+b_ports.attach_kprobe(event=b_ports.get_syscall_fnname("sys_connect"), fn_name="trace_socket")
 
-    struct sk_buff *skb = (struct sk_buff *)PT_REGS_PARM2(ctx);
-    struct iphdr *ip = bpf_hdr_pointer(skb, 0);
-    data.rx_bytes = ip ? skb->len : 0;
+def monitor_processes_and_ports(duration_minutes):
+    iterations = 0
+    duration_seconds = duration_minutes * 60
 
-    struct tcphdr *tcp = (struct tcphdr *)(ip + 1);
-    data.tx_bytes = tcp ? skb->len - (ip->ihl << 2) - (tcp->doff << 2) : 0;
-    data.local_port = tcp ? ntohs(tcp->source) : 0;
+    print("%-6s %-16s %-6s" % ("PID", "COMMAND", "TGID"))
+    b["events"].open_perf_buffer(process_event)
+    
+    print("\nPortas em uso:")
+    print("%-6s %-6s %-6s" % ("PID", "DPORT", "RET"))
+    b_ports["events"].open_perf_buffer(ports_event)
 
-    events.perf_submit(ctx, &data, sizeof(data));
-    return 0;
-}
-"""
-
-bpf = BPF(text=bpf_code)
-bpf.attach_kprobe(event="finish_task_switch", fn_name="monitor_process")
-
-# Infinite loop to keep the eBPF program running
-try:
     while True:
-        bpf.perf_buffer_poll()
-except KeyboardInterrupt:
-    pass
+        try:
+            b.perf_buffer_poll()
+            b_ports.perf_buffer_poll()
+
+            iterations += 1
+            if iterations * 10 >= duration_seconds:
+                break
+            
+            time.sleep(10)  
+
+        except KeyboardInterrupt:
+            break
+
+if __name__ == "__main__":
+    monitor_processes_and_ports(duration_minutes=10)
