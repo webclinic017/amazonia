4bb566b5201b38f3b4fa2428ec7815c1667a3d47
Hudson Coutinho <49199914+Hudsoncoutinho@users.noreply.github.com>
2023-07-27 22:23:08 -0300

    Update entrypoint.sh

--- ebpf/entrypoint.sh
+++ ebpf/entrypoint.sh
@@ -2,6 +2,6 @@
 
 bpftool prog load /app/metrics_ebpf.py /app/metrics_ebpf.o
 
-bpftool cgroup attach /app/metrics_ebpf.o /sys/fs/cgroup/unified/ # Exemplo de anexar a cgroups
+bpftool cgroup attach /app/metrics_ebpf.o /sys/fs/cgroup/unified/
 
 tail -f /dev/null
